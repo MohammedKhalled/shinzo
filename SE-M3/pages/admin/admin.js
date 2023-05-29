9c4b7da4021053bf6ce0db65e63cceea43934298
beshoy isama <besho.osama404@gmail.com>
2023-05-29 23:00:32 +0300

    committed

--- SE-M3/pages/admin/admin.js
+++ SE-M3/pages/admin/admin.js
@@ -3,3 +3,19 @@ logout.addEventListener('click', function() {
   localStorage.removeItem('client');
   window.location.href = '../../index.html';
 });
+
+let acceptBtns = document.querySelectorAll('.solve');
+let rejectBtns = document.querySelectorAll('.ignore');
+
+acceptBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.target.parentElement.innerHTML = '<div><i class="fa-solid fa-circle-check"></i> SOLVED</div>';
+  });
+});
+
+rejectBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.target.parentElement.innerHTML = '<div class="ignored"><i class="fa-solid fa-circle-xmark"></i> IGNORED</div>';
+  });
+}
+);
--- SE-M3/pages/banker/banker.js
+++ SE-M3/pages/banker/banker.js
@@ -3,3 +3,19 @@ logout.addEventListener('click', function() {
   localStorage.removeItem('client');
   window.location.href = '../../index.html';
 });
+
+let acceptBtns = document.querySelectorAll('.accept');
+let rejectBtns = document.querySelectorAll('.reject');
+
+acceptBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.target.parentElement.innerHTML = '<div><i class="fa-solid fa-circle-check"></i> ACCEPTED</div>';
+  });
+});
+
+rejectBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.target.parentElement.innerHTML = '<div class="rejected"><i class="fa-solid fa-circle-xmark"></i> REJECTED</div>';
+  });
+}
+);
